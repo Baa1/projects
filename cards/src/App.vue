3cc39edae1f511401107b54e970225b39d6a36d0
Baa1 <50644318+Baa1@users.noreply.github.com>
2020-10-20 20:09:05 +0700

    Revert "Revert "9""
    This reverts commit 270fa195deff094e588f2c41233b039412e4c764.

--- cards/src/App.vue
+++ cards/src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <!-- <MainContent/> -->
   <router-view>
   </router-view>
 </template>
 
 <script>
-// import MainContent from './components/MainContent'
 export default {
   name: 'App',  
-  // components: {
-  //   MainContent
-  // }
 }
 </script>
 
