b2c9cbf5b28976cf22a47ca270fbeb0eebbcde71
Baa1 <50644318+Baa1@users.noreply.github.com>
2020-10-23 01:09:36 +0700

    14

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,9 +5,26 @@ var sha1 = require('sha1');
 var crypto = require('crypto');
 
 router.post('/authorization', function(req, res) {
-  db.any("SELECT id, login FROM users WHERE login = $1", req.body.login)
+  db.any("SELECT * FROM users WHERE login = $1", req.body.login)
     .then((response) => {
-      res.send(response);
+      if (response && response.length === 1) {
+        if (sha1(req.body.password + response[0].salt) === response[0].password) {
+          res.send({ 
+            userId: response[0].id,
+            message: ""
+          });
+        } else {
+          res.send({
+            userId: 0, 
+            message: "Wrong password" 
+          });
+        }
+      } else {
+        res.send({ 
+          userId: 0, 
+          message: `Couldn't find user with login ${req.body.login}` 
+        });
+      }
     })
     .catch((error) => {
       console.log(error);
