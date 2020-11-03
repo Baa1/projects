e6b1e760fa398c0c020edf46d7c4f8a1324bdec2
Baa1 <50644318+Baa1@users.noreply.github.com>
2020-11-03 11:28:12 +0700

    Update users.js

--- mtg-trade/src/store/modules/users.js
+++ mtg-trade/src/store/modules/users.js
@@ -1,41 +1,41 @@
 import axios from 'axios'
 export default {
   state: {
-      userId: 0,
-      message: ""
+    userId: 0,
+    message: ''
   },
   getters: {
-      USER_ID(state) {
-          return state.userId;
-      },
-      AUTH_MESSAGE(state) {
-          return state.message;
-      }
+    USER_ID (state) {
+      return state.userId
+    },
+    AUTH_MESSAGE (state) {
+      return state.message
+    }
   },
   mutations: {
-      SET_USER_ID: (state, userId) => {
-          state.userId = userId;
-      },
-      SET_MESSAGE: (state, message) => {
-          state.message = message;
-      }
+    SET_USER_ID: (state, userId) => {
+      state.userId = userId
+    },
+    SET_MESSAGE: (state, message) => {
+      state.message = message
+    }
   },
   actions: {
-    async AUTHORIZATION({commit}, params) {
-        let response = await axios.post("http://localhost:3000/users/authorization", params);
-        commit('SET_USER_ID', response.data.userId);
-        commit('SET_MESSAGE', response.data.message);
-        return response.data.message;
+    async AUTHORIZATION ({ commit }, params) {
+      const response = await axios.post('http://localhost:3000/users/authorization', params)
+      commit('SET_USER_ID', response.data.userId)
+      commit('SET_MESSAGE', response.data.message)
+      return response.data.message
     },
-    REGISTRATION({commit}, params) {
-      axios.post("http://localhost:3000/users/registration", params)
+    REGISTRATION ({ commit }, params) {
+      axios.post('http://localhost:3000/users/registration', params)
       .then(response => {
-          commit('SET_USER_ID', response.data.userId);
-          commit('SET_MESSAGE', response.data.message);
+      commit('SET_USER_ID', response.data.userId)
+      commit('SET_MESSAGE', response.data.message)
       })
       .catch(error => {
-          console.log(error);
-      });
+        console.log(error)
+      })
     }
   }
 }
