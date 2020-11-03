a52aafa8118c5eb8715b9f93b7801c555d62cb25
Baa1 <50644318+Baa1@users.noreply.github.com>
2020-11-03 14:28:18 +0700

    4

--- mtg-trade/src/views/Home.vue
+++ mtg-trade/src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="home">
-    <AuthorizationForm/>
-  </div>
+    <div class="home">
+        <AuthorizationForm/>
+    </div>
 </template>
 
 <script>
 import AuthorizationForm from '../components/forms/AuthorizationForm'
 
 export default {
-  name: 'Home',
-  components: {
-    AuthorizationForm
-  }
+    name: 'Home',
+    components: {
+        AuthorizationForm
+    }
 }
 </script>
