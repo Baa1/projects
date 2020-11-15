b6918b3df8da3606e70dc570341861ceba810259
Baa1 <baa1_666@mail.ru>
2020-11-16 04:12:31 +0700

    test 1

--- server/routes/test.js
+++ server/routes/test.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.post('/', (req, res) => {
    console.log(req.body);
    res.send(req.body);
+});
+
+router.get('/file', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Снмок.PNG'));
 });
 
 module.exports = router;
