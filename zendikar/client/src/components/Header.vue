fb69cc463b55722a8d5f300febc19499604a17d3
Baa1 <50644318+Baa1@users.noreply.github.com>
2020-12-17 23:32:15 +0700

    znd components added

---
+++ zendikar/client/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Header">
+    <h1>Zendikar</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.Header {
+  background-color: darkgreen;
+}
+</style>
---
+++ zendikar/client/src/components/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="MainPage">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.MainPage {
+  background-color: darkgreen;
+}
+</style>
