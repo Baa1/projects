cdf9d65d098635a28a7e42f48d7b9a7283264a1b
Baa1 <50644318+Baa1@users.noreply.github.com>
2020-12-24 08:17:19 +0700

    eois_fp roles

--- eois_fp/server/routes/roles.js
+++ eois_fp/server/routes/roles.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const postgres = require('../db/postgres');
 
 router.get('/', (req, res) => {
-  res.send('respond with a resource');
+    let sqlQuery = 'SELECT * FROM roles';
+    postgres.any(sqlQuery)
+        .then(data => {
+            return res.status(200).send({data});
+        })
+        .catch(error => {
+            console.log(error);
+            return res.status(500).send({message: 'Unable to get roles.'});
+        })
 });
 
 module.exports = router;
--- eois_fp/server/routes/users.js
+++ eois_fp/server/routes/users.js
@@ -2,50 +2,65 @@ const express = require('express');
 const router = express.Router();
 const settings = require('../settings');
 const crypto = require('crypto');
+const postgres = require('../db/postgres');
 
 function encrypt(text) { 
-  // Defininf iv 
-  const iv = crypto.randomBytes(16);
-  // Creating Cipheriv with its parameter 
-  let cipher = crypto.createCipheriv(settings.ALGORITHM, Buffer.from(settings.KEY), iv); 
-  // Updating text 
-  let encrypted = cipher.update(text); 
-  // Using concatenation 
-  encrypted = Buffer.concat([encrypted, cipher.final()]); 
-  // Returning iv and encrypted data 
-  return { 
-    iv: iv.toString('hex'),  
-    encryptedData: encrypted.toString('hex') 
-  }; 
+    // Defininf iv 
+    const iv = crypto.randomBytes(16);
+    // Creating Cipheriv with its parameter 
+    let cipher = crypto.createCipheriv(settings.ALGORITHM, Buffer.from(settings.KEY), iv); 
+    // Updating text 
+    let encrypted = cipher.update(text); 
+    // Using concatenation 
+    encrypted = Buffer.concat([encrypted, cipher.final()]); 
+    // Returning iv and encrypted data 
+    return { 
+        iv: iv.toString('hex'),  
+        encryptedData: encrypted.toString('hex') 
+    }; 
 } 
 
  function decrypt(text) { 
-  let iv = Buffer.from(text.iv, 'hex'); 
-  let encryptedText = Buffer.from(text.encryptedData, 'hex'); 
-  // Creating Decipher 
-  let decipher = crypto.createDecipheriv(settings.ALGORITHM, Buffer.from(settings.KEY), iv); 
-  // Updating encrypted text 
-  let decrypted = decipher.update(encryptedText); 
-  decrypted = Buffer.concat([decrypted, decipher.final()]); 
-  // returns data after decryption 
-  return decrypted.toString(); 
+    let iv = Buffer.from(text.iv, 'hex'); 
+    let encryptedText = Buffer.from(text.encryptedData, 'hex'); 
+    // Creating Decipher 
+    let decipher = crypto.createDecipheriv(settings.ALGORITHM, Buffer.from(settings.KEY), iv); 
+    // Updating encrypted text 
+    let decrypted = decipher.update(encryptedText); 
+    decrypted = Buffer.concat([decrypted, decipher.final()]); 
+    // returns data after decryption 
+    return decrypted.toString(); 
 } 
 
-router.get('/register', (req, res) => {
-  let encryptObject = encrypt(req.body.password);
-  let password = encryptObject.encryptedData;
-  let salt = encryptObject.iv;
-  let params = [
+router.post('/register', (req, res) => {
+    let encryptObject = encrypt(req.body.password);
+    let password = encryptObject.encryptedData;
+    let salt = encryptObject.iv;
+    let params = [
     req.body.login,
-    req.body.email,
     password,
     salt,
+    req.body.email,
     req.body.name,
     req.body.surname,
     req.body.patronymic,
     req.body.birthday
-  ];
-  res.send(params);
+    ];
+    let sqlQuery = 'INSERT INTO users (login, password, salt, email, name, surname, patronymic, birthday) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id';
+    postgres.one(sqlQuery, params)
+        .then(data => {
+            console.log(data);
+            return res.status(200).send({message: 'User created.'});
+        })
+        .catch(error => {
+            console.log(error);
+            return res.status(500).send({message: 'Unable create user.'});
+        })
+    res.send(params);
+});
+
+router.post('/authorize', (req, res) => {
+
 });
 
 module.exports = router;
